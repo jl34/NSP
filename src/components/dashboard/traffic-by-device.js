5fe3d61738a68091ab14948a9da222bc448cf38a
Jennifer <Jennifer@Jennifers-MBP.lan>
2023-01-17 23:00:05 -0800

    commit changes

--- src/components/dashboard/traffic-by-device.js
+++ src/components/dashboard/traffic-by-device.js
@@ -1,4 +1,5 @@
-import { Doughnut } from 'react-chartjs-2';
+//import { Doughnut } from 'react-chartjs-2';
+import { Doughnut } from '/Users/Jennifer/Visual Studio/nsp/src/icons/react-chartjs-2';
 import { Box, Card, CardContent, CardHeader, Divider, Typography, useTheme } from '@mui/material';
 
 
