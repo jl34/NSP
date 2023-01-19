e55c9db60e4c415bf18f889bf03bad44af41b566
Jennifer <Jennifer@Jennifers-MBP.lan>
2023-01-18 17:22:05 -0800

    commit changes

--- src/components/dashboard/sales.js
+++ src/components/dashboard/sales.js
@@ -1,4 +1,5 @@
-import { Bar } from 'react-chartjs-2';
+//import { Bar } from 'react-chartjs-2';
+import { Bar } from '/Users/Jennifer/Visual Studio/nsp/src/react-chartjs-2';
 import { Box, Button, Card, CardContent, CardHeader, Divider, useTheme } from '@mui/material';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowRightIcon from '@mui/icons-material/ArrowRight';
@@ -102,7 +103,6 @@ export const Sales = (props) => {
             position: 'relative'
           }}
         >
-          <img src="./Users/Jennifer/Visual Studio/nsp/src/icons/BarChart.png"></img>
            <Bar
             data={data}
             options={options}
