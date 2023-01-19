b1cb33e53a748ccd3c97d3f7b5e2f10bdf15c23a
Jennifer <Jennifer@Jennifers-MBP.lan>
2023-01-18 20:44:09 -0800

    commit changes

--- src/components/dashboard/traffic-by-device.js
+++ src/components/dashboard/traffic-by-device.js
@@ -1,10 +1,8 @@
 import { Doughnut } from 'react-chartjs-2';
 //import { Doughnut } from '/Users/Jennifer/Visual Studio/nsp/src/react-chartjs-2';
 import { Box, Card, CardContent, CardHeader, Divider, Typography, useTheme } from '@mui/material';
-//import {Chart, ArcElement, LineController, LineElement, PointElement, LinearScale, Title} from 'chart.js';
 //import 'chart.js/auto';
 
-Chart.register(Doughnut);
 
 export const TrafficByDevice = (props) => {
   const theme = useTheme();
